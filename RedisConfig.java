e67cff461f4b44830d0d6fb2f4dd92b7d27eca3b
TcLi <254870497@qq.com>
2018-07-13 15:05:00 +0800

    提交

--- RedisConfig.java
+++ RedisConfig.java
@@ -1,42 +1,59 @@
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.lettuce.core.ClientOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import java.time.Duration;
 
 /**
- * @Auther 创建者: Tc李
- * @Date 创建时间: 2018/06/30 15:39
- * @Description 类描述:
+ * Created by lorne on 2017/7/5.
  */
+
 @Configuration
 public class RedisConfig {
+
+    private static Logger logger = LoggerFactory.getLogger(RedisConfig.class);
+
+
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
 
-        LettuceConnectionFactory factory = new LettuceConnectionFactory();
-        factory.setTimeout(1000);
-        factory.setHostName("192.168.0.188");
-        factory.setPort(6379);
-        factory.setDatabase(6);
+        ClientOptions.Builder clientBuilder = ClientOptions.builder();
+
+        clientBuilder.pingBeforeActivateConnection(true);
+
+        RedisStandaloneConfiguration standaloneConfiguration = new RedisStandaloneConfiguration();
+        standaloneConfiguration.setHostName("192.168.0.188");
+        standaloneConfiguration.setPort(6379);
+//        standaloneConfiguration.setPassword(RedisPassword.of("redis"));
+        standaloneConfiguration.setDatabase(6);
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder builder = LettuceClientConfiguration.builder();
+        builder.commandTimeout(Duration.ofMinutes(6));
+        builder.shutdownTimeout(Duration.ofMinutes(6));
+        builder.clientOptions(clientBuilder.build());
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(standaloneConfiguration,builder.build());
         return factory;
 
     }
 
-    @Bean
-    public RedisTemplate redisTemplate(){
 
+    @Bean
+    public RedisTemplate<String,Object> getRedisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         ObjectMapper map = new ObjectMapper();
 
         map.setVisibility(PropertyAccessor.ALL,JsonAutoDetect.Visibility.ANY);
-        map.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
 
         Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
         jackson2JsonRedisSerializer.setObjectMapper(map);
@@ -48,7 +65,7 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
 
         redisTemplate.setConnectionFactory(redisConnectionFactory());
+        
         return redisTemplate;
     }
-
 }
